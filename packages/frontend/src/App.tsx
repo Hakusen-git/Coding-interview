d8132662c7d93213d066d9587cf4db837cc96a42
myao209 <63892309+Hakusen-git@users.noreply.github.com>
2022-05-24 02:07:40 +1200

    Update TodoList in App.tsx

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Box } from "@mui/material";
 import "./App.css";
 import Header from "./components/Header/Header";
-import { AllTodoList } from "./hooks/todos/useGetTodos";
+import { TodoList } from "./components/TodoList/TodoList";
 
 function App() {
   return (
     <div className="App">
       <Box component={"div"} sx={{ boxShadow: "6", maxHeight: "80%" }}>
         <Header />
-        <AllTodoList />
+        <TodoList />
       </Box>
     </div>
   );
